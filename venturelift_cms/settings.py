589c06de379e6b4cb1b155829260d29a9526ed52
Brayonski <kiprotichbrian8@gmail.com>
2020-04-10 09:41:52 +0300

    update

--- venturelift_cms/settings.py
+++ venturelift_cms/settings.py
@@ -60,7 +60,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
